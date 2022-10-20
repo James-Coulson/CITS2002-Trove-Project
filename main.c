0a65fb9356f79a0ba5d8827674f09c22540d3003
James-Coulson <78469310+James-Coulson@users.noreply.github.com>
2022-10-20 13:07:25 +0800

    feat: finished search function

--- main.c
+++ main.c
@@ -148,6 +148,53 @@ int populate_trovefile(TROVEFILE *trovefile, FILE *fp) {
 	return 0;
 }
 
+// -------------------- SEARCH TROVEFILE METHDO -------------------- //
+
+int search_trovefile() {
+	// Attempting to open trovefile
+	FILE *fp = fopen(fvalue, "r");
+
+	// Checking file was found
+	CHECK_NULL(fp);
+
+	// Defining TROVEFILE
+	TROVEFILE *trovefile = calloc(1, sizeof(TROVEFILE));
+
+	// Populating trovefile
+	populate_trovefile(trovefile, fp);
+
+	// Closing file
+	fclose(fp);
+
+	// Get word
+	char *word = filelist[filelist_index];
+
+	// Defining found boolean
+	bool found = false;
+
+	// Iterate through entries
+	for (int i = 0; i < trovefile->num_entries; i++) {
+		// Getting entry
+		TROVEFILE_ENTRY *entry = &trovefile->entries[i];
+
+		// Iterating through words
+		for (int j = 0; j < entry->num_words; j++) {
+			// Check if words are same
+			if (strcmp(word, entry->words[j]) != 0) { continue; }
+
+			// Print to output
+			printf("Found matching word in: %s\n", entry->file_path);
+
+			// Setting found to true
+			found = true;
+		}
+	}
+
+	// Returning
+	if (found) { return 0; }
+	else {return false; }
+}
+
 // -------------------- MODIFY TROVEFILE METHODS -------------------- //
 
 int modify_trovefile() {
@@ -245,8 +292,6 @@ int modify_trovefile() {
 		// Populating trovefile
 		populate_trovefile(trovefile, fp);
 
-		// printf("%i\n", trovefile->num_entries);
-
 		// Closing file
 		fclose(fp);
 
@@ -269,12 +314,6 @@ int modify_trovefile() {
 				break;
 			}
 		}
-
-		// // // Print
-		// for (int i = 0; i < trovefile->entries[0].num_words; i++) {
-		// 	printf("%s | ", trovefile->entries[0].words[i]);
-		// }
-		// printf("\n");
 
 		// Saving trovefile
 		return save_trovefile(trovefile);
@@ -318,9 +357,6 @@ int modify_trovefile() {
 		}
 
 		// --- Update new files --- //
-
-		// Defining file pointer
-		FILE *fp;
 
 		// Iterating through filelist
 		for (int i = filelist_index; i < filelist_length; i++) {
@@ -422,14 +458,18 @@ int main(int argc, char *argv[]) {
 	// Checking fflag is raised
 	if (!fflag) {
 		printf("No file was given in the arg (use -f)\n");
-		return 0;
+		return 1;
 	}
 
 	// Checking if modifying trovefile
 	if (bflag || rflag || uflag) {
-		// Calling modify_trovefile and returning exist status
+		// Calling modify_trovefile and returning exit status
 		return modify_trovefile();
+	} else {
+		// Calling search_trovefile and returning exit status
+		return search_trovefile();
 	}
 
-	return 0;
+	// If we are here something has gone wrong
+	return 1;
 }
